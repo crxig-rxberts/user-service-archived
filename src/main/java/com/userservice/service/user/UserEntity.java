44c18a0cdcf1089c4fbeeda598cc3508ad41ce39
Craig Roberts <craig26031996@gmail.com>
2023-02-27 10:12:02 +0000

    WIP-controller/response_enhancmenets

--- src/main/java/com/userservice/service/user/UserEntity.java
+++ src/main/java/com/userservice/service/user/UserEntity.java
@@ -1,7 +1,13 @@
 package com.userservice.service.user;
 
 
-import lombok.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 import org.springframework.stereotype.Component;
 
 import javax.persistence.Entity;
